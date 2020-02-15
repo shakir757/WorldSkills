d6d27c63288d494b20b311539de9cce9bcf7d018
WSR-1126 <wsr-1126@iMac-WS-Video.local>
2020-02-15 10:09:16 +0300

    StartActivity

---
+++ app/src/main/java/com/example/converter/StartActivity.java
@@ -0,0 +1,32 @@
+package com.example.converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Context context = getApplicationContext();
+        SharedPreferences prefs = context.getSharedPreferences("myAppPrefs", Context.MODE_PRIVATE);
+        Boolean isOnboardingPresented = prefs.getBoolean("isOnboardingPresented", false);
+
+        if (!isOnboardingPresented) {
+            Intent intent1 = new Intent(this, MainActivity.class);
+            startActivity(intent1);
+            prefs.edit().putBoolean("isOnboardingPresented", true).apply();
+        } else {
+            Intent intent2 = new Intent(this, ConverterActivity.class);
+            startActivity(intent2);
+        }
+
+    }
+}
