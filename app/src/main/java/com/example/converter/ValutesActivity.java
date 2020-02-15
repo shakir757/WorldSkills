e72ab74e85d00d573c919792bf418278aae98805
WSR-1126 <wsr-1126@iMac-WS-Video.local>
2020-02-15 16:10:43 +0300

    Converter Activity complete

--- app/src/main/java/com/example/converter/ValutesActivity.java
+++ app/src/main/java/com/example/converter/ValutesActivity.java
@@ -3,16 +3,12 @@ package com.example.converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class ConverterActivity extends AppCompatActivity {
+public class ValutesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_converter);
-    }
-
-    public void gotoValutes(View view) {
+        setContentView(R.layout.activity_valutes);
     }
 }
