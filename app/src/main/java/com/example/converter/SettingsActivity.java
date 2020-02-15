9331ebd3af46233855fcaaa4e8e6c544d3cb5f21
WSR-1126 <wsr-1126@iMac-WS-Video.local>
2020-02-15 17:51:42 +0300

    Change language on page settings

--- app/src/main/java/com/example/converter/SettingsActivity.java
+++ app/src/main/java/com/example/converter/SettingsActivity.java
@@ -5,17 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    TextView language, info, menuC, menuS, version, header;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        language = findViewById(R.id.textViewChangeLanguage);
+        info = findViewById(R.id.textViewInfo);
+        menuC = findViewById(R.id.textViewConvert1);
+        menuS = findViewById(R.id.textViewSettings1);
+        version = findViewById(R.id.textViewVerison);
+        header = findViewById(R.id.textViewHeader);
     }
 
     public void gotoConverter(View view) {
         Intent intent = new Intent(this, ConverterActivity.class);
         startActivity(intent);
+    }
+
+    public void changelanguage(View view) {
+        if (i == 0) {
+            language.setText("Поменять язык");
+            info.setText("Информация");
+            menuC.setText("Конвертировать");
+            menuS.setText("Настройки");
+            version.setText("версия приложения 1.0");
+            header.setText("Настройки");
+            i = 1;
+        } else {
+            language.setText("Change Language");
+            info.setText("Info");
+            menuC.setText("Convert");
+            menuS.setText("Settings");
+            version.setText("app version 1.0");
+            header.setText("Settings");
+            i = 0;
+        }
     }
 }
